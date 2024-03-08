a06c74e373235002bb29e6347503229d250ac19c
1919SaiRam <vijaykumarchinta1919@gmail.com>
2024-03-09 00:02:10 +0530

    main

--- xcountriesminibuild/src/App.js
+++ xcountriesminibuild/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./styles.css";
+import "./App.css";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
